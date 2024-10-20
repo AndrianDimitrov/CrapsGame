04dfcb0a912e89e4dfeae809db97d9f9ca7c8fc3
AndrianDimitrov <andriandimitrov02gmail.com>
2024-10-20 21:30:39 +0300

    Added Unit tests

--- src/main/java/com/example/crapsGame/data/constants/CrapsConstants.java
+++ src/main/java/com/example/crapsGame/data/constants/CrapsConstants.java
@@ -1,6 +1,6 @@
 package com.example.crapsGame.data.constants;
 
-public class CrapConstants {
+public class CrapsConstants {
     public static final String WIN_MSG = "Win";
     public static final String LOSE_MSG = "Lose";
     public static final String CRAPS_MSG = "Craps";
